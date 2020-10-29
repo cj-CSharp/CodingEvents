d7afb8419d035c35c28937f84d01d326f9143cb9
cjconnor18 <cjallen1988@gmail.com>
2020-10-29 12:25:09 -0500

    Created EventData in Data Folder to hold and edit data. completed video

--- CodingEvents/Controllers/EventsController.cs
+++ CodingEvents/Controllers/EventsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CodingEvents.Data;
 using CodingEvents.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,13 +11,11 @@ namespace CodingEvents.Controllers
     public class EventsController : Controller
     {
 
-        private static List<Event> Events = new List<Event>();
-
         //GET: /<controllers>/
         [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.events = Events;
+            ViewBag.events = EventData.GetAll();
 
             return View();
         }
@@ -31,7 +30,7 @@ namespace CodingEvents.Controllers
         [Route("/events/add")]
         public IActionResult NewEvent(string name, string description)
         {
-            Events.Add(new Event(name, description));
+            EventData.Add(new Event(name, description));
 
             return Redirect("/Events");
         }
