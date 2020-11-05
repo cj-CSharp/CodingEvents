effe080a45c479133d0a24f39cd04140108ac38b
cjconnor18 <cjallen1988@gmail.com>
2020-11-05 11:48:12 -0600

    added ViewModel Validation

--- CodingEvents/ViewModels/AddEventViewModel.cs
+++ CodingEvents/ViewModels/AddEventViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,15 @@ namespace CodingEvents.ViewModels
 {
     public class AddEventViewModel
     {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(10, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(500, ErrorMessage = "Description is too long!")]
         public string Description { get; set; }
+
+        [EmailAddress]
+        public string ContactEmail { get; set; }
     }
 }
